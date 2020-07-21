f3742664dc34e45a5363a220de613bc788ed6b77
Fizx <kazanfizuku@gmail.com>
2020-07-21 17:20:40 +0800

    Updated token

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 exports.youtube =  "AIzaSyBJOwDhd1yoWRiSVlF72Hw4Ysmj2nNbC9U";
 exports.default_prefix = "l.";
-exports.token = process.env.TOKEN
+exports.token = "Njk4NTI5MTYwOTM4NzgyNzIw.XwWieA.NLn8ucZ-_4tFbXJwXaDLu_7y_No"
