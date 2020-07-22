a27c5ba3248c8fb07cae543202173abe80e5d0d3
Fizx <kazanfizuku@gmail.com>
2020-07-22 09:26:02 +0800

    Token change

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 exports.youtube =  "AIzaSyBJOwDhd1yoWRiSVlF72Hw4Ysmj2nNbC9U";
 exports.default_prefix = "l.";
-exports.token = "Njk4NTI5MTYwOTM4NzgyNzIw.XwWieA.NLn8ucZ-_4tFbXJwXaDLu_7y_No"
+exports.token = "Njk4NTI5MTYwOTM4NzgyNzIw.XxeVbA.eV2-xnYEBAwUuDHAcyfKMAvrGb0"
